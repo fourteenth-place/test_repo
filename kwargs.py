10cbd6ce50701cda638ad716a5ea0282b2bceb50
disbr007 <disbr007@umn.edu>
2023-03-15 18:52:21 -0400

    fix

--- kwargs.py
+++
@@ -1,9 +0,0 @@
-
-
-def calling_func(arg)
-
-
-def inner_func(arg, tm):
-    print(arg)
-    print(tm)
-    
--- test_env_vars.py
+++ test_env_vars.py
@@ -1,7 +1,8 @@
-import os; 
-val = os.environ.get('RUN_ENV')
+import os
+
+val = os.environ.get("RUN_ENV")
 if val is None:
-    print('RUN_ENV is None')
+    print("RUN_ENV is None")
     # raise Exception('VAL IS NONE')
 else:
-    print(f'python found env var: {val}')
+    print(f"python found env var: {val}")
